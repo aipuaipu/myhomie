25f58526a8391364a831de7cba397041336e7a66
anymater <aipuhub@gmail.com>
2026-09-03 22:24:59 +0800

    chore: deploy legacy keyboard migration

---
+++ assets/Confirm-C9zK6eB_.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./Confirm.vue_vue_type_script_setup_true_lang-GRnA6GH_.js";import"./index-DV3T_Za9.js";export{m as default};
--- assets/Confirm-CjSHAc_7.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./Confirm.vue_vue_type_script_setup_true_lang-DdwfTMSO.js";import"./index-BPVAJu06.js";export{m as default};
